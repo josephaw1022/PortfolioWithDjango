f7a0c6d1aac326dfa92d400329245c043e7c6496
josephaw1022 <josephsims1@gmail.com>
2021-06-08 15:33:12 -0400

    add  frontend

---
+++ pages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+
+class Index(TemplateView):
+    template_name = "pages/index.html"
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,4 @@
-"""business URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('pages.urls'))
 ]
